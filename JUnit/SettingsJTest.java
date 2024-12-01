d0c6afba9834a627c1cf3295f56554301ee111de
Julian Sharpe <jsharp55@uwo.ca>
2024-11-30 23:33:07 -0500

    Changed JUnit test for settings class

--- JUnit/SettingsJTest.java
+++ JUnit/SettingsJTest.java
@@ -51,32 +51,29 @@ public class SettingsJTest {
     @Test
     public void testLoadSettings() {
         System.out.println("loadSettings");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
+        Settings instance = new Settings(TEST_FILE_NAME);
 
-        assertEquals("TestPet", instance.getPetName());
         assertTrue(instance.getBackgroundMusic());
-        assertFalse(instance.isParent());
         assertEquals(30, instance.getStudyTime());
         assertEquals(10, instance.getBreakTime());
-        assertEquals(5, instance.getTargetStudyTime());
     }
 
     @Test
     public void testSaveToFile() {
         System.out.println("saveToFile");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
+        Settings instance = new Settings(TEST_FILE_NAME);
         instance.setBreakTime(20);
         instance.saveToFile(TEST_FILE_NAME);
 
         // Reload settings to verify changes persisted
-        Settings reloaded = new Settings("TestPet",TEST_FILE_NAME);
+        Settings reloaded = new Settings(TEST_FILE_NAME);
         assertEquals(20, reloaded.getBreakTime());
     }
 
     @Test
     public void testSetAndGetStudyTime() {
         System.out.println("setStudyTime and getStudyTime");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
+        Settings instance = new Settings(TEST_FILE_NAME);
         instance.setStudyTime(45);
         assertEquals(45, instance.getStudyTime());
     }
@@ -84,38 +81,23 @@ public class SettingsJTest {
     @Test
     public void testSetAndGetBreakTime() {
         System.out.println("setBreakTime and getBreakTime");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
+        Settings instance = new Settings(TEST_FILE_NAME);
         instance.setBreakTime(15);
         assertEquals(15, instance.getBreakTime());
     }
 
     @Test
-    public void testSetAndGetTargetStudyTime() {
-        System.out.println("setTargetStudyTime and getTargetStudyTime");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
-        instance.setTargetStudyTime(8);
-        assertEquals(8, instance.getTargetStudyTime());
-    }
-
-    @Test
     public void testSetAndGetBackgroundMusic() {
         System.out.println("setBackgroundMusic and getBackgroundMusic");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
+        Settings instance = new Settings(TEST_FILE_NAME);
         instance.setBackgroundMusic(false);
         assertFalse(instance.getBackgroundMusic());
     }
 
     @Test
-    public void testIsParent() {
-        System.out.println("isParent");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
-        assertFalse(instance.isParent());
-    }
-
-    @Test
     public void testToString() {
         System.out.println("toString");
-        Settings instance = new Settings("TestPet",TEST_FILE_NAME);
+        Settings instance = new Settings(TEST_FILE_NAME);
         String expected = "Settings for Pet: TestPet\n" +
                 "Background Music: On\n" +
                 "Is Parent: No\n" +
